618a42146c2e41debe8189ab6dbd230986676237
Your Name <527rgaballa@frhsd.com>
2026-04-28 08:39:32 -0400

    css

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
-let tasks =[] //Empty array to store tasks
+let tasks = [] //Empty array to store tasks
 
 document.getElementById('addTaskBtn').addEventListener('click', function () {
-
-let taskInput = document.getElementById('taskInput').value
-
-if(taskInput){
-
+  //Get the value from INput field
+  let taskInput = document.getElementById('taskInput').value
+  //check if Input is empty
+  if (taskInput) {
+    //add new task to task array
     tasks.push(taskInput)
-
+    //clear input field value
     document.getElementById('taskInput').value = ''
-
-        displayTasks()
-    }
+    //update Task List Display
+    displayTasks()
+  }
 })
 
 function displayTasks () {
+  //Select our TaskList in the HTML
+  let taskList = document.getElementById('taskList')
+  //Clear the existing HTML List
+  taskList.innerHTML = ''
+  //loop through each Task in the array and create a list item for each
+  tasks.forEach((task, index) => {
+    //Create <li> element for each task
+    let li = document.createElement('li')
 
-let taskList = document.getElementById('tasklist')
-
-taskList.innerHTML = ''
-
-tasks.forEach((task, index)=> {
-    
-    let li = document.createElement ('li')
-
-
+    //add Styling
     li.classList.add(
-        'list-group-item',
-        'd-flex',
-        'justify-content-between',
-        'align-items-center'
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-center'
     )
-
-li.innerHTML = ${task} <button class='Btn btn-success btn-sm' onclick='removeTask(${index})'>✔</button>
-
-taskList.appendChild(li)
-})
-
-    
-
-
+    //Set innerHTML of the LI with a task and remove btn
+    li.innerHTML = `${task} <button class='btn btn-success btn-sm' onclick='removeTask(${index})'>✔</button>`
+    //Append the new task list to the HTML
+    taskList.appendChild(li)
+  })
+  
 }
+
+function removeTask(index){
+  tasks.splice(index,1)
+  displayTasks()
+}
+
+
+document.getElementById('clearTasksBtn').addEventListener('click', function () {
+  tasks = []
+  displayTasks()
+})
